0fa83069f28966663388c23a9ac003b6958501e8
Michał Pomirski <michal.pomirski01@gmail.com>
2024-02-11 15:54:42 +0100

    test commit

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,13 +1,14 @@
 module.exports = {
-    // Type check TypeScript files
-    '**/*.(ts|tsx)': () => 'npm tsc --noEmit',
+  // Type check TypeScript files
+  '**/*.(ts|tsx)': () => 'npm tsc --noEmit',
 
-    // Lint & Prettify TS and JS files
-    '**/*.(ts|tsx|js)': filenames => [
-        `npm eslint ${filenames.join(' ')}`,
-        `npm prettier --write ${filenames.join(' ')}`
-    ],
+  // Lint & Prettify TS and JS files
+  '**/*.(ts|tsx|js)': (filenames) => [
+    `npm eslint ${filenames.join(' ')}`,
+    `npm run prettier --write ${filenames.join(' ')}`,
+  ],
 
-    // Prettify only Markdown and JSON files
-    '**/*.(md|json)': filenames => `npm prettier --write ${filenames.join(' ')}`
+  // Prettify only Markdown and JSON files
+  '**/*.(md|json)': (filenames) =>
+    `npm run prettier --write ${filenames.join(' ')}`,
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
-
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello! My name's Michał.<div className="">_</div></h1>
+      <h1>
+        Hello! My name's Michał.<div className="flex">_</div>
+      </h1>
     </main>
   );
 }
