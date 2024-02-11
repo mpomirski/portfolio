3256f02473b95fc483394aaee7b2e0d7de9c68c3
Michał Pomirski <michal.pomirski01@gmail.com>
2024-02-11 15:49:36 +0100

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Press_Start_2P } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const presStart2P = Press_Start_2P({
+  weight: '400',
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={presStart2P.className}>{children}</body>
     </html>
   );
 }
