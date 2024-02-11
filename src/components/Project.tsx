593413fc31d9b27a622fdaaaf9cb410dab6f033b
mpomirski <michal.pomirski01@gmail.com>
2024-02-11 23:41:24 +0100

    Add Project component

---
+++ src/components/Project.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Project: React.FC = () => {
+  return (
+    <div>
+      <h2>Project</h2>
+      <p>Project content</p>
+    </div>
+  );
+};
+export default Project;
