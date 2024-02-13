7fa10fa39dc36397c1bf06c5d132f5b4a557592b
mpomirski <michal.pomirski01@gmail.com>
2024-02-13 12:13:40 +0100

    Fixed crt effect not extending on small screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Header from '@/components/Header';
 
 export default function Home() {
   return (
-    <div className="content px-1 min-h-full">
+    <div className="content px-1 min-h-[calc(100vh-1rem)] relative">
       <Header />
       <main className="min-h-32">
         <Introduction />
